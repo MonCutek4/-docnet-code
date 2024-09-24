f6d2bb44d65580f92a852b88d566513c798fa7ab
MonCutek4 <hoathanh125c@gmail.com>
2024-09-24 08:50:42 +0700

    dotnet

---
+++ repos/webkobejp/webkobejp/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webkobejp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Shop()
+        {
+            return View();
+        }
+        public ActionResult ShopDetail()
+        {
+            return View();
+        }
+        public ActionResult cart()
+        {
+            return View();
+        }
+        public ActionResult chackout()
+        {
+            return View();
+
+        }
+        public ActionResult testimonial()
+        {
+            return View();
+
+        }
+        public ActionResult Error()
+        {
+            return View();
+
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+      
+    }
+}
